63a0b19830233247969e5779f9371c7955fee9f6
mtrivinom <mtrivinom@uoc.edu>
2022-12-20 07:54:39 +0100

    Cambios Configuración y Servicio

---
+++ src/app/Services/songslist.service.spec.ts
@@ -0,0 +1,16 @@
+import { TestBed } from '@angular/core/testing';
+
+import { SongslistService } from './songslist.service';
+
+describe('SongslistService', () => {
+  let service: SongslistService;
+
+  beforeEach(() => {
+    TestBed.configureTestingModule({});
+    service = TestBed.inject(SongslistService);
+  });
+
+  it('should be created', () => {
+    expect(service).toBeTruthy();
+  });
+});
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -4,7 +4,7 @@ export const environment = {
   firebase: {
     apiKey: "AIzaSyDPgzoGtyyMeV2tiT54B8u3JFu1NzgAI98",
     authDomain: "pinpilinpauxa-p4.firebaseapp.com",
-    databaseURL: "https://pinpilinpauxa-p4-default-rtdb.europe-west1.firebasedatabase.app",
+    databaseURL: "https://pinpilinpauxa-p4.firebaseio.com",
     projectId: "pinpilinpauxa-p4",
     storageBucket: "pinpilinpauxa-p4.appspot.com",
     messagingSenderId: "53499176909",
